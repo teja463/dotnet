e336f4a6f04ce89a35cbc3edeab21be74474139b
teja463 <teja463@gmail.com>
2024-12-06 12:33:13 +0530

    added .net framework project and updated readme

---
+++ DotnetFrameworkConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotnetFrameworkConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(".Net Framework hello world");
+        }
+    }
+}
---
+++ DotnetFrameworkConsoleApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DotnetFrameworkConsoleApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Collins Aerospace")]
+[assembly: AssemblyProduct("DotnetFrameworkConsoleApp")]
+[assembly: AssemblyCopyright("Copyright © Collins Aerospace 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("679cbcc2-e915-49a2-9757-91bf5c7d5c5b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
