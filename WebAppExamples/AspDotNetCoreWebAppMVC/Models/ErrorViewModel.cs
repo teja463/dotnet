8622d7898203beff7007a110f1e1e7840ddedbb8
teja463 <teja463@gmail.com>
2024-12-12 13:02:25 +0530

    added mvc, razor and blazor web applications

---
+++ WebAppExamples/AspDotNetCoreWebAppMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspDotNetCoreWebAppMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
